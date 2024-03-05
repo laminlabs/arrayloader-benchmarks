3f8877ef30205a72ac8007f154a3d7cd3ba5dd0e
Chaichontat Sriworarat <csriwor1@jh.edu>
2024-03-05 09:51:47 -0500

    Enable row/col setup

--- setup.py
+++ setup.py
@@ -1,69 +1,85 @@
 # %%
 import h5py
 import lamindb as ln
+import rich_click as click
 import scanpy as sc
 import tiledbsoma.io
+from loguru import logger
 
-# %%
 BATCH_SIZE = 128
-ln.setup.close()
-ln.setup.load("laminlabs/arrayloader-benchmarks")
-artifact = ln.Artifact.filter(uid="z3AsAOO39crEioi5kEaG").one()
 
-# %%
-artifact.view_lineage()
 
-# %%
-artifact.describe()
+@click.command()
+@click.option("--nrows", type=int, default=-1)
+@click.option("--ncols", type=int, default=5000)
+def main(nrows: int | None, ncols: int | None):
+    if nrows == -1:
+        nrows = None
+    if ncols == -1:
+        ncols = None
 
-# %%
-# we will save in different formats, so no need to cache
-with artifact.backed() as store:
-    adata = store[:, :5000].to_memory()  # ~2GB sparse and ~4.6GB dense stored as h5ad
-# %%
-adata.write_h5ad("adata_benchmark_sparse.h5ad")
-adata.write_zarr("adata_benchmark_sparse.zrad")
+    ln.setup.close()
+    ln.setup.load("laminlabs/arrayloader-benchmarks")
 
-# %%
-tiledbsoma.io.from_h5ad(
-    "adata_benchmark_sparse.soma",
-    input_path="adata_benchmark_sparse.h5ad",
-    measurement_name="RNA",
-)
+    artifact = ln.Artifact.filter(uid="z3AsAOO39crEioi5kEaG").one()
+    logger.info("Artifact: {}", artifact)
+    artifact.view_lineage()
+    artifact.describe()
 
-# %% Dense onwards
-adata.X = adata.X.toarray()
+    # %%
+    # we will save in different formats, so no need to cache
+    logger.info("Loading data from S3")
+    with artifact.backed() as store:
+        # ~2GB sparse and ~4.6GB dense stored as h5ad
+        adata = store[:nrows, :ncols].to_memory()
+    # %%
+    adata.write_h5ad("adata_benchmark_sparse.h5ad")
+    adata.write_zarr("adata_benchmark_sparse.zrad")
 
-adata.write_h5ad("adata_benchmark_dense.h5ad")
-adata.write_zarr("adata_benchmark_dense.zrad")
-adata.write_zarr(
-    f"adata_benchmark_dense_chunk_{BATCH_SIZE}.zrad",
-    chunks=(BATCH_SIZE, adata.X.shape[1]),
-)
+    # %%
+    tiledbsoma.io.from_h5ad(
+        "adata_benchmark_sparse.soma",
+        input_path="adata_benchmark_sparse.h5ad",
+        measurement_name="RNA",
+    )
 
-# %%
-# save h5 with dense chunked X, no way to do it with adata.write_h5ad
-with h5py.File(f"adata_dense_chunk_{BATCH_SIZE}.h5", mode="w") as f:
-    f.create_dataset(
-        "adata",
-        adata.X.shape,
-        dtype=adata.X.dtype,
-        data=adata.X,
+    # %% Dense onwards
+    adata.X = adata.X.toarray()
+
+    adata.write_h5ad("adata_benchmark_dense.h5ad")
+    adata.write_zarr("adata_benchmark_dense.zrad")
+    adata.write_zarr(
+        f"adata_benchmark_dense_chunk_{BATCH_SIZE}.zrad",
         chunks=(BATCH_SIZE, adata.X.shape[1]),
     )
-    labels = adata.obs.cell_states.cat.codes.to_numpy()
-    f.create_dataset("labels", labels.shape, data=labels)
 
-# %%
-df_X_labels = sc.get.obs_df(adata, keys=adata.var_names.to_list() + ["cell_states"])
+    # %%
+    # save h5 with dense chunked X, no way to do it with adata.write_h5ad
+    with h5py.File(f"adata_dense_chunk_{BATCH_SIZE}.h5", mode="w") as f:
+        f.create_dataset(
+            "adata",
+            adata.X.shape,
+            dtype=adata.X.dtype,
+            data=adata.X,
+            chunks=(BATCH_SIZE, adata.X.shape[1]),
+        )
+        labels = adata.obs.cell_states.cat.codes.to_numpy()
+        f.create_dataset("labels", labels.shape, data=labels)
 
-# %%
-# default row groups
-df_X_labels.to_parquet("adata_dense.parquet", compression=None)
+    # %%
+    df_X_labels = sc.get.obs_df(adata, keys=adata.var_names.to_list() + ["cell_states"])
 
-# %%
-df_X_labels.to_parquet(
-    f"adata_dense_chunk_{BATCH_SIZE}.parquet",
-    compression=None,
-    row_group_size=BATCH_SIZE,
-)
+    # %%
+    # default row groups
+    df_X_labels.to_parquet("adata_dense.parquet", compression=None)
+
+    # %%
+    df_X_labels.to_parquet(
+        f"adata_dense_chunk_{BATCH_SIZE}.parquet",
+        compression=None,
+        row_group_size=BATCH_SIZE,
+    )
+
+
+if __name__ == "__main__":
+    main()
