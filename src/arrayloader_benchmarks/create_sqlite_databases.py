132b9d8afa60734419bc4907f6e5d26885660e53
Felix Fischer <fe.fischer@icloud.com>
2025-07-18 17:47:04 +0200

    🐛 Fixed syntax error + prettified queries

--- src/arrayloader_benchmarks/create_sqlite_databases.py
+++ src/arrayloader_benchmarks/create_sqlite_databases.py
@@ -8,23 +8,22 @@ DB_PATH = Path("/dss/mcmlscratch/04/di93zer/benchmarks.sqlite")
 
 def create_store_table(db_path: Path):
     """Create SQLite database with a table to store benchmark results."""
-
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute(
         """
-           CREATE TABLE IF NOT EXISTS benchmarks
-           (
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
-               store_path TEXT NOT NULL,
-               store_type TEXT NOT NULL,
-               gene_space TEXT NOT NULL,
-               zarr_chunk_size INTEGER NOT NULL,
-               zarr_shard_size INTEGER NOT NULL,
-               anndata_shard_size INTEGER NOT NULL,
-               creation_time REAL NOT NULL,
-           )
-            """
+        CREATE TABLE IF NOT EXISTS benchmarks
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            store_path TEXT NOT NULL,
+            store_type TEXT NOT NULL,
+            gene_space TEXT NOT NULL,
+            zarr_chunk_size INTEGER NOT NULL,
+            zarr_shard_size INTEGER NOT NULL,
+            anndata_shard_size INTEGER NOT NULL,
+            creation_time REAL NOT NULL
+        )
+        """
     )
     conn.commit()
     conn.close()
@@ -32,55 +31,24 @@ def create_store_table(db_path: Path):
 
 def create_results_table(db_path: Path):
     """Create SQLite database with a table to store benchmark results."""
-
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("""
-                   CREATE TABLE IF NOT EXISTS benchmarks
-                   (
-                       id
-                       INTEGER
-                       PRIMARY
-                       KEY
-                       AUTOINCREMENT,
-                       store_path
-                       TEXT
-                       NOT
-                       NULL,
-                       store_type
-                       TEXT
-                       NOT
-                       NULL,
-                       n_samples
-                       INTEGER
-                       NOT
-                       NULL,
-                       batch_size
-                       INTEGER
-                       NOT
-                       NULL,
-                       use_torch_loader
-                       BOOLEAN
-                       NOT
-                       NULL,
-                       chunk_size
-                       INTEGER
-                       NOT
-                       NULL,
-                       preload_nchunks
-                       INTEGER
-                       NOT
-                       NULL,
-                       num_workers
-                       INTEGER
-                       NOT
-                       NULL,
-                       samples_per_sec
-                       REAL
-                       NOT
-                       NULL
-                   )
-                   """)
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS benchmarks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            store_path TEXT NOT NULL,
+            store_type TEXT NOT NULL,
+            n_samples INTEGER NOT NULL,
+            batch_size INTEGER NOT NULL,
+            use_torch_loader BOOLEAN NOT NULL,
+            chunk_size INTEGER NOT NULL,
+            preload_nchunks INTEGER NOT NULL,
+            num_workers INTEGER NOT NULL,
+            samples_per_sec REAL NOT NULL
+        )
+        """
+    )
     conn.commit()
     conn.close()
 
