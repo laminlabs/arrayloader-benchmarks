138db5ab2d985788371932487d2e1b17b9d71082
Felix Fischer <fe.fischer@icloud.com>
2025-09-15 11:18:54 +0200

    🎨 Updated script to run benchmark

--- src/arrayloader_benchmarks/arrayloaders/run_benchmark.py
+++ src/arrayloader_benchmarks/arrayloaders/run_benchmark.py
@@ -1,21 +1,37 @@
 from __future__ import annotations
 
 import warnings
-from pathlib import Path
 
 import anndata as ad
 import click
+import lamindb as ln
+import numpy as np
 import scipy.sparse as sp
 import zarr
 import zarrs  # noqa
 from arrayloaders import ZarrSparseDataset
 from torch.utils.data import DataLoader
+from torch.utils.dlpack import from_dlpack
 
 from arrayloader_benchmarks.utils import benchmark_loader
 
 zarr.config.set(
     {"codec_pipeline.path": "zarrs.ZarrsCodecPipeline", "threading.max_workers": None}
 )
+
+# Suppress zarr vlen-utf8 codec warnings
+warnings.filterwarnings(
+    "ignore",
+    message="The codec `vlen-utf8` is currently not part in the Zarr format 3 specification.*",
+    category=UserWarning,
+    module="zarr.codecs.vlen_utf8",
+)
+
+
+def collate_fn(elems):
+    batch_x = sp.vstack([v[0] for v in elems])
+    batch_obs = np.concatenate([v[1] for v in elems])
+    return from_dlpack(batch_x), from_dlpack(batch_obs)
 
 
 @click.command()
@@ -27,27 +43,29 @@ zarr.config.set(
 @click.option("--batch_size", type=int, default=4096)
 @click.option("--n_samples", type=int, default=-1)
 def benchmark(  # noqa: PLR0917
-    store_path: str,
     chunk_size: int = 256,
-    preload_nchunks: int = 4,
+    preload_nchunks: int = 8,
     use_torch_loader: bool = True,  # noqa: FBT001, FBT002
     num_workers: int = 8,
     batch_size: int = 4096,
-    n_samples: int = -1,
+    n_samples: int = 2_000_000,
 ):
-    store_path = Path(store_path)
+    # Download store from laminHub
+    store_shards = ln.Collection.get("LaJOdLd0xZ3v5ZBw0000").cache()
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        ds = ZarrSparseDataset(
-            shuffle=True,
-            chunk_size=chunk_size,
-            preload_nchunks=preload_nchunks,
-            batch_size=1 if use_torch_loader else batch_size,
-        )
-        ds.add_datasets(
-            [ad.io.sparse_dataset(zarr.open(p)["X"]) for p in store_path.glob("*.zarr")]
-        )
+    ds = ZarrSparseDataset(
+        shuffle=True,
+        chunk_size=chunk_size,
+        preload_nchunks=preload_nchunks,
+        batch_size=1 if use_torch_loader else batch_size,
+    )
+    ds.add_datasets(
+        datasets=[ad.io.sparse_dataset(zarr.open(p)["X"]) for p in store_shards],
+        obs=[
+            ad.io.read_elem(zarr.open(p)["obs"])["cell_line"].to_numpy()
+            for p in store_shards
+        ],
+    )
 
     n_samples = n_samples if n_samples != -1 else len(ds)
     if use_torch_loader:
@@ -56,7 +74,7 @@ def benchmark(  # noqa: PLR0917
             batch_size=batch_size,
             num_workers=num_workers,
             drop_last=True,
-            collate_fn=lambda x: sp.vstack([v[0] for v in x]),
+            collate_fn=collate_fn,
         )
         samples_per_sec, _, _ = benchmark_loader(loader, n_samples, batch_size)
     else:
